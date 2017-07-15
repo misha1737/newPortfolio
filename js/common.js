2a7ff06b6124650fb696b0a9f983ddd84c84db3f
misha <misha1737@yandex.ru>
2017-07-15 22:36:01 +0300

    переключення між сторінками

--- js/common.js
+++ js/common.js
@@ -1,5 +1,24 @@
 $(document).ready(function() {
+// var flag=true;
 
-	
-	
+    $("#main").click(function()
+        {
+        	 $("section").css({'display':'none'});
+            $(".main").css({'display':'block'});
+    });	
+    $("#portfolio").click(function()
+        {
+        	 $("section").css({'display':'none'});
+            $(".portfolio").css({'display':'block'});
+    });	
+    $("#skills").click(function()
+        {
+        	 $("section").css({'display':'none'});
+            $(".skills").css({'display':'block'});
+    });	
+    $("#contacts").click(function()
+        {
+        	 $("section").css({'display':'none'});
+            $(".contacts").css({'display':'block'});
+    });	
 });
