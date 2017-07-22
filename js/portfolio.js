fafd1ab7079beade553afb12d48140da2293d9d4
misha <misha1737@yandex.ru>
2017-07-23 01:05:31 +0300

    портфоліо технологии

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,77 +1,77 @@
 var blockrender=function(){
- 
 
 
-var arrSite = $(".siteIcon");
-var sliderWidth =$("#slider").width();
+    var arrSite = $(".siteIcon");
+    var sliderWidth =$("#slider").width();
 
+    var offset=function(number){
 
-console.log(arrSite);
-//elem =arrSite.shift();
-var offset=function(number){
+      return (number*180-180);
 
-    //     number=arrSite.length+position;
-    //     position=0;
-    //     console.log(position);
-    // }
-  return (number*180-180);
-
-}
-
-
-for(var i=0; i<arrSite.length;i++){   
-    if (i==0 || i==arrSite.length-1 ){
-        $("#slider .siteIcon:eq("+i+")").css({
-            'opacity':'0.0'
-
-        });
-        
-    }else{
-        $("#slider .siteIcon:eq("+i+")").css({
-            'opacity':'1'
-
-        });
     }
 
-$("#slider .siteIcon:eq("+i+")").css({'left':offset(i)+'px'});
 
-} 
+    for(var i=0; i<arrSite.length;i++){   
+        if (i==0 || i==arrSite.length-1 ){
+            $("#slider .siteIcon:eq("+i+")").css({'opacity':'0.0'});
+        }
+        else{
+            $("#slider .siteIcon:eq("+i+")").css({'opacity':'1'});
+        }
+
+    $("#slider .siteIcon:eq("+i+")").css({'left':offset(i)+'px'});
+
+    } 
 }
 
 $(document).ready(function() {
 var elem;
 blockrender();
 
- 
 
-
- // $("#portfolio").click(function()
- //        {
- //           console.log(sliderWidth);
- //    }); 
 
  $("#nextSlide").click(function()
         {
-            //position++;
             elem=$('#slider > :first').detach(); 
             elem.appendTo('#slider');
             blockrender();
-            
-
     }); 
  $("#prevSlide").click(function()
         {
             elem=$('#slider > :last').detach(); 
             elem.prependTo('#slider');
             blockrender();
-            //$('.siteIcon > :first').detach(); 
-            // elem.appendTo('.siteIcon');
     }); 
 
+$(".siteIcon").click(function()
+        {
+            var siteBox= $(this).attr('id');
+            var siteNumber=($(this).attr('id')).substring(4);
+// Виділення іконок сайтів          
+            $('.siteIcon img').css ({
+                 'box-shadow':'none'
+            });
+            $("#"+siteBox +" img").css ({
+                'box-shadow':'0px 0px 40px #474d5d inset'
+            });
+//---------------------------
 
+//очистка інформації про сайт
+            
+            for(var i=0;i<$(".siteIcon").length;i++){
+                $(".siteText"+i).css({
+                    'display':'none'
+                }); 
+            }
+           
+//---------------------------
 
-
-
-
+//добавлення інформації про сайт
+            $(".siteText"+siteNumber).css({
+                    'display':'inline-block'
+                 }); 
+             console.log( (".site"+4));
+//---------------------------
+    }); 
 
     });	
